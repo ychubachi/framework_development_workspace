96f2028f03211bcf82a65927af2675a8991f9bb0
Yoshihide Chubachi <yoshi@chubachi.net>
2012-12-16 02:33:58 +0900

    Square2とSquare3を差し替えました

--- Square2/src/square/Square.java
+++ Square2/src/square/Square.java
@@ -12,29 +12,17 @@ public class Square {
 
 	private void run() throws IOException {
 		// 自乗を計算する値を入力する
-		double value = input();
-		// 自乗を計算する
-		double result = process(value);
-		// 計算結果を出力する
-		output(result);
-	}
-
-	private double input() throws IOException {
 		System.out.print("自乗を計算する値を入力してください：");
 		BufferedReader reader =
 			new BufferedReader(
 				new InputStreamReader(System.in));
 		String valueString = reader.readLine();
 		double value = Double.parseDouble(valueString);
-		return value;
-	}
-
-	private double process(double value) {
+		
+		// 自乗を計算する
 		double result = value * value;
-		return result;
-	}
-
-	private void output(double result) {
+		
+		// 計算結果を出力する
 		System.out.println("計算結果：" + result);
 	}
 }
--- Square3/src/square/Square.java
+++ Square3/src/square/Square.java
@@ -12,17 +12,29 @@ public class Square {
 
 	private void run() throws IOException {
 		// 自乗を計算する値を入力する
+		double value = input();
+		// 自乗を計算する
+		double result = process(value);
+		// 計算結果を出力する
+		output(result);
+	}
+
+	private double input() throws IOException {
 		System.out.print("自乗を計算する値を入力してください：");
 		BufferedReader reader =
 			new BufferedReader(
 				new InputStreamReader(System.in));
 		String valueString = reader.readLine();
 		double value = Double.parseDouble(valueString);
-		
-		// 自乗を計算する
+		return value;
+	}
+
+	private double process(double value) {
 		double result = value * value;
-		
-		// 計算結果を出力する
+		return result;
+	}
+
+	private void output(double result) {
 		System.out.println("計算結果：" + result);
 	}
 }
